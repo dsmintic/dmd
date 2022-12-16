e1b499f744ab89fdc5579899067ded8ae4ba6a72
dsmintic <dsmintic@gmail.com>
2022-12-16 10:21:37 +0100

    Video folder

--- src/components/RecipeContent.vue
+++ src/components/RecipeContent.vue
@@ -6,48 +6,83 @@ export default {
 
 <template>
 
-    <section class="container">
-        <article>
-            <h1 class="fontbold">Title 1</h1>
-            <img src="../images/vine.png">
-            <p class="textfont">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis mollitia officiis ad
-                unde perspiciatis ratione. Voluptate commodi dignissimos distinctio. Animi in est eligendi corporis
-                deleniti
-                magnam qui ad culpa voluptate?</p>
-        </article>
-    </section>
+  <section class="container">
+    <article>
+      <h1 class="fontbold">Biscuit Eggs</h1>
+      <img src="../images/BiscuitEggs.png">
+      <p>The butterless biscuits for these Benedicts are perfect for kids to make because they are so simple. For little
+        and big kids: Let them help with measuring, mixing and cutting out the biscuits and making the eggless
+        hollandaise.</p>
+      <p>Ingredients
+        Biscuits:<br>
+        120g all-purpose flour<br>
+        3 tablespoons chopped fresh chives, plus more for garnish<br>
+        1 tablespoon baking powder<br>
+        1 tablespoon sugar<br>
+        1 1/2 teaspoons kosher salt, plus more for sprinkling
+        170g fresh goat cheese, crumbled
+        240ml heavy cream, plus more for brushing
+        Quick Hollandaise:
+        115g mayonnaise
+        1 tablespoon fresh lemon juice
+        1/8 teaspoon paprika
+        Kosher salt and freshly ground black pepper
+        8 slices Canadian bacon
+        8 large eggs
+      </p>
+      <p class="textfont">Method
+        For the biscuits: Position an oven rack in the center of the oven, and preheat to 220 degrees C. Line a baking
+        sheet with parchment.
+        Whisk together the flour, chives, baking powder, sugar and salt in a large bowl. Add the goat cheese, and toss,
+        using your hands to break it up into pea-sized pieces. Add the cream, and mix with a wooden spoon until
+        combined. Let the dough stand for 5 minutes.
+        Roll the dough out into a round that is slightly less than 1/2 inch thick. Cut out 8 biscuits with a 2 1/2-inch
+        round cookie cutter, and arrange them about 2 inches apart on the prepared baking sheet. Brush them with more
+        cream, and sprinkle with a little salt. Bake the biscuits until just golden, 16 to 18 minutes. Transfer them to
+        a wire rack, and let cool. When the biscuits are cool enough to handle, split them in half. Put 2 bottom halves
+        on each of 4 plates.
+        For the quick hollandaise: Whisk the mayonnaise, lemon juice, paprika and 1 tablespoon water in a small bowl,
+        and season with salt and pepper; set aside.
+        Heat a large nonstick skillet over medium-high heat. Cook the bacon in batches until browned, 3 to 4 minutes per
+        side. Place 1 slice of bacon on each biscuit bottom, folding if needed.
+        Bring a wide pot with about 2 inches of water to a very gentle simmer. Break the eggs into the water, spacing
+        them apart. Cover the pot, and turn off the heat. After 2 to 3 minutes the whites should be set and the yolks
+        still soft. Remove the eggs with a slotted spoon, and place on top of the Canadian bacon slices. Drizzle with
+        hollandaise, sprinkle with chives and sandwich each with a biscuit top
+      </p>
+    </article>
+  </section>
 
 </template>
 
 <style scoped>
-
 .container {
-    overflow: hidden;
-    display: flex;
-    justify-content: space-around;
-    align-items: flex-start;
+  overflow: hidden;
+  display: flex;
+  justify-content: space-around;
+  align-items: flex-start;
 }
 
-.container article{
+.container article {
   margin-left: 10px;
   margin-right: 10px;
   margin-bottom: 20px;
   flex-basis: 33%;
 }
 
-.container article img{
+.container article img {
   width: 100%;
   display: block;
 }
 
-.container article p{
+.container article p {
   font-size: medium;
   background-color: #FFF6ED;
 }
 
 
 .fontbold {
-  background-color:peachpuff;
+  background-color: peachpuff;
   padding: 20px;
   font-weight: 700;
   margin: 0;
