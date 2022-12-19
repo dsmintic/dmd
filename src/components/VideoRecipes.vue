5c458f3e41b1b6f098484a57ec37d6b503a72f7d
dsmintic <dsmintic@gmail.com>
2022-12-19 14:02:19 +0100

    Video page

--- src/components/VideoRecipes.vue
+++ src/components/VideoRecipes.vue
@@ -1,25 +1,86 @@
 <template>
+    <!-- <h1 class="fontbold"><router-link :to="{ name: 'Video' }">Find your recipe in this video</router-link></h1> -->
+
     <section class="container">
         <article>
-            <h1 class="fontbold"><router-link :to="{ name: 'Video' }">Find your recipe in this video</router-link></h1>
-
-            <div>
-            <video  width="400" controls >
+            <h4 class="fontbold">Video 1</h4>
+            <video width="400" controls>
                 <source src="../video/coloreggs.mp4" type="video/mp4">
-            </video></div>
-            <div>
-            <video  width="400" controls >
-                <source src="../video/eggvideo.mp4" type="video/mp4">
-            </video></div>
-            <div>
-            <video width="400" controls >
-                <source src="../video/allRecipes.mp4" type="video/mp4">
-            </video></div>
+            </video>
+            <p class="textfont">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto quam veniam a,
+                cumque, excepturi non
+                exercitationem temporibus maiores ipsam quod provident recusandae quos, eligendi ea saepe suscipit rerum
+                ipsa sapiente?</p>
+        </article>
 
+
+        <article>
+            <h4 class="fontbold">Video 2</h4>
+            <video width="400" controls>
+                <source src="../video/eggvideo.mp4" type="video/mp4">
+            </video>
+            <p class="textfont">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto quam veniam a,
+                cumque, excepturi non
+                exercitationem temporibus maiores ipsam quod provident recusandae quos, eligendi ea saepe suscipit rerum
+                ipsa sapiente?</p>
+        </article>
+
+        <article>
+            <h4 class="fontbold">Video 3</h4>
+            <video width="400" controls>
+                <source src="../video/eggvideo.mp4" type="video/mp4">
+            </video>
             <p class="textfont">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto quam veniam a,
                 cumque, excepturi non
                 exercitationem temporibus maiores ipsam quod provident recusandae quos, eligendi ea saepe suscipit rerum
                 ipsa sapiente?</p>
         </article>
     </section>
+
+    <section class="container">
+        <article>
+            <h4 class="fontbold">Video 4</h4>
+            <video width="400" controls>
+                <source src="../video/allRecipes.mp4" type="video/mp4">
+            </video>
+            <p class="textfont">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto quam veniam a,
+                cumque, excepturi non
+                exercitationem temporibus maiores ipsam quod provident recusandae quos, eligendi ea saepe suscipit rerum
+                ipsa sapiente?</p>
+        </article>
+
+    </section>
+
+
+
+
 </template>
+
+<style scoped>
+.container {
+    overflow: hidden;
+    display: flex;
+    justify-content: space-around;
+    align-items:flex-start;
+}
+
+.container article {
+    margin-left: 10px;
+    margin-right: 10px;
+    margin-bottom: 20px;
+    flex-basis: 50%;
+}
+
+.fontbold {
+    background-color: peachpuff;
+    padding: 20px;
+    font-weight: 700;
+    margin: 0;
+}
+
+.textfont {
+    font-weight: 100;
+    padding: 10px;
+    margin: 0;
+}
+</style>
