359602e8c4d2ed5ecf9a12f49773ab1b092788da
dsmintic <dsmintic@gmail.com>
2022-12-20 10:20:10 +0100

    add form update

--- src/components/RecipeForm.vue
+++ src/components/RecipeForm.vue
@@ -19,17 +19,24 @@
                 <p><label for="title">Title:</label> <input type="text" id="title"
                         placeholder="Please enter the name of the recipe" v-model="title"></p>
 
-                <p><label for="summary">Summary:</label> <input type="text" id="summary"
-                        placeholder="Please enter a summary of the recipe" v-model="summary"></p>
+                <!-- <p><label for="summary">Summary:</label> <input type="text" id="summary"
+                        placeholder="Please enter a summary of the recipe" rows="10" v-model="summary"></p> -->
+
+                <p><label for="title">Summary:</label>
+                    <textarea class="textarea" placeholder="Please enter a summary of the recipe" rows="10"></textarea>
+                </p>
 
                 <!-- Dennis - Ingredients adding + - -->
 
                 <p><label for="title">Method:</label>
-                <textarea class="textarea" placeholder="Please enter the cooking method" rows="20"></textarea>
+                    <textarea class="textarea" placeholder="Please enter the cooking method" rows="20"></textarea>
                 </p>
 
+                <div class="container_buttons">
+                    <p><input class="myButton" type="submit" value="Add recipe"></p>
+                </div>
 
-                <p><input type="submit" value="Add recipe"></p>
+
             </form>
         </article>
     </section>
@@ -38,25 +45,30 @@
 
 <style scoped>
 .container {
-  overflow: hidden;
-  display: flex;
-  justify-content: center;
-  /* background-color: #FFF6ED; */
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+
 }
 
 .container article {
-  margin-left: 10px;
-  margin-right: 10px;
-  margin-bottom: 20px;
-  background-color: #FFF6ED;
-  width: 1000px;
-  /* flex-basis: 100%; */
+    margin-left: 10px;
+    margin-right: 10px;
+    margin-bottom: 20px;
+    background-color: #FFF6ED;
+    width: 1000px;
+    /* flex-basis: 100%; */
 }
 
 .container article h1 {
-  font-size: medium;
-  background-color: #FFF6ED;
+    font-size: medium;
+    background-color: #FFF6ED;
 }
+
+.container article p {
+    font-size: medium;
+}
+
 
 :focus-visible {
     outline-color: #FFBF86;
@@ -64,49 +76,93 @@
     border-color: #FFDAB9;
     border-style: solid;
 }
-#formRecipe input[type=text], #formRecipe textarea {
+
+#formRecipe input[type=text],
+#formRecipe textarea {
     width: 50%;
-    background-color: #FFF6ED;
+    background-color: white;
+    height: 25px;
+    border-bottom: 1px solid black;
+    box-shadow: inset 2px 2px 2px grey, 0 -1px 1px white;
 }
+
 #formRecipe textarea {
     height: 200px;
     resize: none;
     border: 3px solid transparent;
-    box-shadow: inset 0 1px 2px rgba(0,0,0,4);
-    background-color: #FFF6ED;
-    width: 100%;
+    /* box-shadow: inset 0 1px 2px rgba(0, 0, 0, 4); */
+    box-shadow: inset 2px 2px 2px grey, 0 -1px 1px white;
+    background-color: white;
+    width: 80%;
 }
-.textareaLabel {
-    vertical-align: top;
-}
+
+
 .error {
     background-color: #FDB9FF;
 }
+
 #formRecipe label {
     display: inline-block;
     width: 100px;
     text-align: right;
+
 }
 
 .inset {
-   border: 3px solid transparent;
-   /* border-bottom: 2px solid black;
-   box-shadow: inset 2px 2px 2px grey, 0 -1px 1px white; */
-   box-shadow: inset 0 1px 2px rgba(0,0,0,4);
-   height: 25px;
-   background-color: #FFF6ED;
-  }
+    /* border: 3px solid transparent;
+   box-shadow: inset 0 1px 2px rgba(0,0,0,4); */
+    /* border-bottom: 1px solid black; */
+    box-shadow: inset 2px 2px 2px grey, 0 -1px 1px white;
+    width: 220px;
+    height: 30px;
+    text-align: center;
+    background-color: white;
+    margin-left: 100px;
+}
 
-  .fontbold {
-  background-color: peachpuff;
-  padding: 20px;
-  font-weight: 700;
-  margin: 0;
+.fontbold {
+    background-color: peachpuff;
+    padding: 20px;
+    font-weight: 700;
+    margin: 0;
 }
 
 .textfont {
-  font-weight: 100;
-  padding: 10px;
-  margin: 0;
+    font-weight: 100;
+    padding: 10px;
+    margin: 0;
+}
+
+.container_buttons {
+    overflow: hidden;
+    display: flex;
+    justify-content: space-between;
+    padding-bottom: 10px;
+    margin-bottom: 0;
+}
+
+.myButton {
+    box-shadow: 0px 10px 14px -7px #276873;
+    margin-top: 5px;
+    background-color: #B9DEFF;
+    border-radius: 8px;
+    display: inline-block;
+    cursor: pointer;
+    color: black;
+    font-size: 20px;
+    font-weight: bold;
+    padding: 13px 25px;
+    text-decoration: none;
+    border: none;
+}
+
+.myButton:hover {
+    background-color: #FFDAB9;
+    color: white;
+}
+
+.myButton:active {
+    position: relative;
+    top: 2px;
 }
 </style>
